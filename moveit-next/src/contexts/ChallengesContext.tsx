ed48b055bb611ed562976008b7e3576f693f6864
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2021-02-28 12:20:29 -0300

    Add challenges context file

---
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useState, ReactNode } from 'react';
+import challenges from "../../challenges.json";
+
+interface Challenge {
+    type: 'body' | 'eye',
+    description: string,
+    amount: number,
+}
+
+interface ChallengesContextData {
+    level: number,
+    currentExperience: number,
+    challengesCompleted: number,
+    activeChallenge: Challenge,
+    experienceToNextLevel: number,
+    levelUp: () => void,
+    startNewChallenge: () => void,
+    resetChallenge: () => void,
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+interface ChallengesProviderProps {
+    children: ReactNode
+}
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+    const [level, setLevel] = useState(1);
+    const [currentExperience, setCurrentExperience] = useState(0);
+    const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState(null);
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2); // o 4 representa o fator de experiência, que pode ser aumentado ou diminuído, para que uppar se tornar mais fácil ou mais difícil.
+
+    function levelUp() {
+        setLevel(level + 1);
+    }
+
+    function startNewChallenge() {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null);
+    }
+
+    return (
+        <ChallengesContext.Provider
+            value={{
+                level,
+                currentExperience,
+                challengesCompleted,
+                levelUp,
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge,
+                experienceToNextLevel,
+            }}
+        >
+            {children}
+        </ChallengesContext.Provider>
+    );
+}
