8aa07527b0217b3a98ad66d66b219a8738b54916
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2021-02-28 12:16:07 -0300

    Update home file

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -15,14 +16,14 @@ export default function Home() {
       <ExperienceBar />
 
       <section>
+
         <div>
           <Profile />
           <CompletedChallenges />
           <Countdown />
         </div>
-
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
